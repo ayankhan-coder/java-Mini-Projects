1d1dd3b9317ecf9e4edfc7e17e29eac8a9f80da0
ayankhan-coder <ayankhan786490@gmail.com>
2026-06-07 18:49:46 +0530

    second commit

--- src/projects/StudentManagement.java
+++ src/projects/StudentManagement.java
@@ -23,74 +23,80 @@ public class StudentManagement {
 
             System.out.println("--------------------------------------------------------------------------------");
 
-            System.out.print("Enter Your option : ");
+            System.out.print("$ Enter Your option : ");
 
             try {
                 option = sc.nextInt();
             } catch (Exception e) {
-                System.out.println("Enter Correct Option Number");
+                System.out.println("~~~~~~ Enter Correct Option Number ~~~~~~\n");
+                sc.nextLine();
+                continue;
+
             }
 
             switch (option) {
                 case 1:
-                    System.out.println("Enter Student name to Add :");
+                    System.out.println("$ Enter Student name to Add :");
                     sc.nextLine();
                     String addName = sc.nextLine();
                     students.add(addName);
-                    System.out.println("Name Added");
+                    System.out.println("~~~~~~ NAME ADDED ~~~~~~\n");
                     break;
 
                 case 2:
-                    System.out.println("Enter The Name to Delete :");
+                    System.out.println("$ Enter The Name to Delete :");
                     sc.nextLine();
                     String deleteName = sc.nextLine();
                     if (students.contains(deleteName)) {
                         students.remove(deleteName);
-                        System.out.println("Name Deleted");
+                        System.out.println("~~~~~~ NAME DELETED ~~~~~~\n");
                     } else {
-                        System.out.println("Name Does not Exists");
+                        System.out.println("~~~~~~ Name Does not Exists ~~~~~~\n");
                     }
                     break;
 
                 case 3:
-                    System.out.println("Enter Name To Search : ");
+                    System.out.println("$ Enter Name To Search : ");
                     sc.nextLine();
                     String nameSearch = sc.nextLine();
                     boolean search = students.contains(nameSearch);
                     if (search == true) {
                         System.out.println(
-                                nameSearch + " Exists At Positon " +
-                                        students.indexOf(nameSearch)
+                                "~~~~~~ "+nameSearch + " Exists At Positon " +
+                                        students.indexOf(nameSearch)+"~~~~~~~"
                         );
 
                     } else {
-                        System.out.println(nameSearch + " Does not Exists");
+                        System.out.println("~~~~~~ "+nameSearch + " Does not Exists"+" ~~~~~~\n");
                     }
                     break;
 
                 case 4:
-                    System.out.println("Students List");
-                    System.out.println(students);
+                    System.out.println("~~~~~~ Students List ~~~~~~");
+                    for (String element:students){
+                        System.out.println(students.indexOf(element)+". "+element);
+                    }
+                    System.out.println("\n");
                     break;
 
                 case 5:
-                    System.out.println(students.size() + " Students Exists");
+                    System.out.println("~~~~~~ "+students.size() + " Students Exists ~~~~~~\n");
                     break;
 
                 case 6:
-                    System.out.print("Old Student Name : ");
+                    System.out.print("$ Old Student Name : ");
                     sc.nextLine();
                     String oldName = sc.nextLine();
-                    System.out.print("New Student Name : ");
+                    System.out.print("$ New Student Name : ");
                     String newName = sc.nextLine();
                     if (students.contains(oldName)) {
                         students.set(
                                 students.indexOf(oldName),
                                 newName
                         );
-                        System.out.println("Updated Sucsussfull");
+                        System.out.println("~~~~~~ UPDATED SUCCESSFUL ~~~~~~\n");
                     } else {
-                        System.out.println("That old name does not exists");
+                        System.out.println("~~~~~~ That old name does not exists ~~~~~~\n");
                     }
                     break;
 
@@ -98,9 +104,8 @@ public class StudentManagement {
                     return;
 
                 default:
-                    System.out.println("Invalid option");
+                    System.out.println("~~~~~~ INVALID OPTION ~~~~~~\n");
             }
         }
-
     }
 }
